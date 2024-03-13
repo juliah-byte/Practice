f46935f73c947a7d926cb5ddddced59b19bca9e2
juliah-byte <79874878+juliah-byte@users.noreply.github.com>
2024-03-12 23:52:30 -0400

    adding makeOutWord method

--- src/practice/interview.java
+++ src/practice/interview.java
@@ -4,14 +4,49 @@ public class interview {
 	
 	public static void main (String[] args){
 		
-		int myloop = 10;
+		Boolean emptyBoolean;
+		
+		//int myloop = 10;
 		System.out.println("Testing");
 		
-		for(int i = 0; i < myloop; i++) {
+		/**for(int i = 0; i < myloop; i++) {
 			
 			System.out.println("Test Passed");
-		}
+		}**/
+		
+		//System.out.println(sleepIn(true, true));
+		
+		emptyBoolean = sleepIn(true, false);
+		
+		System.out.println(emptyBoolean);
 	}
 	
-
+	
+	public static boolean sleepIn(boolean weekday, boolean vacation) {
+		
+		
+		if (weekday == true && vacation == false) {
+			
+			return false;
+		}
+		
+		else if (weekday == false && vacation == false) {
+			
+			return true;
+		}
+		
+		else if (weekday == false && vacation == true) {
+			
+			return true;
+		}
+		
+		return true;
+	}
+	
+	
+	public String makeOutWord() {
+		
+		return "string";
+	}
+	
 }
